de97cd6aec6cca062f0da600817003f89f1371e6
liul85 <liul85@gmail.com>
2021-02-02 22:10:49 +1100

    support Darwin

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,14 +1,30 @@
 #!/usr/bin/env bash
 set -e
 
-echo "Installing ohmyzsh"
-sudo dnf -y install zsh
+os_type=$(uname)
+echo "Current os is $os_type"
+
+echo "Installing zsh..." 
+case $os_type in
+    Linux)
+        sudo dnf -y install zsh
+        ;;
+    Darwin)
+        brew install zsh
+        ;;
+    *)
+        echo "Unsupported os type: $os_type"
+        echo "zsh was not installed."
+        exit 1
+esac
+
+echo "Installing oh-my-zsh..."
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 
-echo "Installing fzf"
-git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-~/.fzf/install
+#echo "Installing fzf"
+#git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+#~/.fzf/install
 
 # link all dotfiles
 all_files=$(ls -al | awk -F " " '{print $9}' | grep -i "^\.[A-Za-z].*" | grep -vi "^.git$")
@@ -22,7 +38,7 @@ done
 
 for f in $all_files
 do
-    ls -al /home/$USER/$f
+    ls -al ~/$f
 done
 
 echo "done!"
