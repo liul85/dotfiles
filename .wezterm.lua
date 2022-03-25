f71f853598151df95fa211e81f267b4ca53e9a1c
liul85 <liul85@gmail.com>
2022-03-25 21:03:06 +1100

    Update config

---
+++ .wezterm.lua
@@ -0,0 +1,11 @@
+local wezterm = require 'wezterm';
+
+return {
+  font = wezterm.font("Noto Sans Mono"),
+  font_size = 12,
+  color_scheme = "Gruvbox Dark",
+  hide_tab_bar_if_only_one_tab = true,
+  window_padding = {
+    bottom = 0,
+  }
+}
